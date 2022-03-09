2e91d91c0bc9a00c7a43802855cddaf31dde69df
RenatadeCastroAraujo <renata_castroaraujo@yahoo.com.br>
2022-03-09 15:57:48 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,59 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      disabled: true,
+    };
+  }
+
+  buttonAbled = () => {
+    const { name } = this.state;
+    const MAGICNUMBER = 2;
+    if (name.length >= MAGICNUMBER) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
+  inputSearch = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, this.buttonAbled);
+  }
+
   render() {
+    const { name, disabled } = this.state;
     return (
       <div data-testid="page-search">
-        <h3>Search</h3>
         <Header />
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+              id="search"
+              name="name"
+              value={ name }
+              onChange={ this.inputSearch }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disabled }
+            onClick={ () => {} }
+          >
+            Procurar
+          </button>
+        </form>
       </div>
 
     );
